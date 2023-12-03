2df59ff6a3fe0be2de2bb2df0a7a59a2b8cc48a5
Michelle Stuart <michelle.stuart@rutgers.edu>
2023-12-03 08:41:28 -0500

    validate fields when creating invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,16 +18,18 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-
-export async function createInvoice(formData: FormData) {
-  //   const { customerId, amount, status } = CreateInvoice.parse({
-  //     customerId: formData.get('customerId'),
-  //     amount: formData.get('amount'),
-  //     status: formData.get('status'),
-  //   });
-  const { customerId, amount, status } = CreateInvoice.parse(
+  const validatedFields = CreateInvoice.safeParse(
     Object.fromEntries(formData.entries()),
   );
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+  console.log('validatedFields', validatedFields);
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
   // console.log(customerId, amountInCents, status, date);
