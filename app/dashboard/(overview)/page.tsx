aedc98bcdc9c908d57c34b50171bfba2cfa35d60
Michelle Stuart <michelle.stuart@rutgers.edu>
2025-05-14 15:01:51 -0400

    add metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,8 +4,11 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '../../ui/fonts';
 import { Suspense } from 'react';
 import { CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
 
- 
+ export const metadata: Metadata = {
+  title: 'Dashboard',
+}
 export default async function Page() {
   
   return (
